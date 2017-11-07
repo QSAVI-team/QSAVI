c62c6699a1d9eb21d5b3f69035b31df5cc60220d
Martin Bigger <0mb60@queensu.ca>
2017-11-06 19:39:09 -0500

    variable delimiter

--- Unity/hand import/Assets/RotateCube.cs
+++ Unity/hand import/Assets/RotateCube.cs
@@ -5,7 +5,7 @@ using System.IO.Ports;
 
 public class RotateCube : MonoBehaviour {
     //public string port=""; 
-	SerialPort sp = new SerialPort("COM6", 115200);
+	SerialPort sp = new SerialPort("COM3", 115200);
     float[] lastRotation = { 0, 0, 0 };
     float[] caliRotation = { 0, 0, 0 }; // position pointing down at your side
 
@@ -20,29 +20,48 @@ public class RotateCube : MonoBehaviour {
         Debug.Log(line);
         string[] vec3 = line.Split(',');//split the line at each tab (this is how the code currently formats the output)
         Debug.Log(vec3);
-/*
-        if (vec3[0] == "0" && vec3[1] == "0" && vec3[2] == "0")
+
+        if (vec3[0] != "" && vec3[1] != "" && vec3[2] != "") //check that no values are blank
         {
-            while (vec3[0] == "0" && vec3[1] == "0" && vec3[2] == "0")
-            { }
+            if (vec3[0] == "2") // delimiter representing calibration
+            {
+                while (vec3[0] == "2") // wait until button pressed ends
+                { }
+
+                caliRotation[0] = float.Parse(vec3[1]);
+                caliRotation[1] = float.Parse(vec3[2]);
+                caliRotation[2] = float.Parse(vec3[3]);
+                Debug.Log(caliRotation);
+            }
+            if (vec3[0] == "1") // delimiter representing rotation
+            {
+                transform.Rotate(                           //parse each value from a string to a float
+                        float.Parse(vec3[0]) - lastRotation[0], // - caliRotation[0],
+                        float.Parse(vec3[1]) - lastRotation[1], //- caliRotation[1],
+                        float.Parse(vec3[2]) - lastRotation[2], //- caliRotation[2],
+                        Space.Self
+                        );
+                lastRotation[0] = float.Parse(vec3[0]); //set new values for the most recent rotation
+                lastRotation[1] = float.Parse(vec3[1]);
+                lastRotation[2] = float.Parse(vec3[2]);
+                sp.BaseStream.Flush();
+            }
+            /*
+            if (vec3[0] == "0")// delimiter representing translation
+            {
+                transform.Translate(
+                    float.Parse(vec3[0]) - lastPosition[0], // - caliRotation[0],
+                        float.Parse(vec3[1]) - lastPosition[1], //- caliRotation[1],
+                        float.Parse(vec3[2]) - lastPosition[2], //- caliRotation[2],
+                        Space.Self
+                        );
+                lastPosition[0] = float.Parse(vec3[0]); //set new values for the most recent rotation
+                lastPosition[1] = float.Parse(vec3[1]);
+                lastPosition[2] = float.Parse(vec3[2]);
+                sp.BaseStream.Flush();
+            }
+             */   
+            
         }
-        caliRotation[0] = float.Parse(vec3[0]);
-        caliRotation[1] = float.Parse(vec3[1]);
-        caliRotation[2] = float.Parse(vec3[2]);
-        */
-        if (vec3[0] != "" && vec3[1] != "" && vec3[2] != "")//check that no values are blank
-        {
-            transform.Rotate(                           //parse each value from a string to a float
-                float.Parse(vec3[0]) - lastRotation[0],// - caliRotation[0],
-                float.Parse(vec3[1]) - lastRotation[1] ,//- caliRotation[1],
-                float.Parse(vec3[2]) - lastRotation[2] ,//- caliRotation[2],
-                Space.Self
-                );
-            lastRotation[0] = float.Parse(vec3[0]);//set new values for the most recent rotation
-            lastRotation[1] = float.Parse(vec3[1]);
-            lastRotation[2] = float.Parse(vec3[2]);
-            sp.BaseStream.Flush();
-        }
-       
 	}
 }
