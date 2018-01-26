15842c370ce2bf1401697c13436999dffad34ba4
vacer25 <vacer25@gmail.com>
2018-01-25 19:33:53 -0500

    Added central Arduino data acquisition in "ArduinoInterface.cs" class
    Instead of each object reading data from the COM port, only the "Arduino
    Interface" object does that and stores the returned data in a public
    static string. All other scripts that need access to Arduino data simply
    get data from the shared static string in the "ArduinoInterface.cs"
    class.

---
+++ Unity/hand import/Assets/Scripts/ArduinoInterface.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoInterface : MonoBehaviour {
+
+	// ------------------------- CONSTANT SERIAL PORT DATA -------------------------
+
+	[Header("COM Port")]
+	public string setCOMPort = "COM3";
+	public int setCOMBaud = 115200;
+
+    public static string COM_PORT;
+	public static int COM_BAUD;
+
+	// ------------------------- CONSTANT TIMING DATA -------------------------
+
+	[Header("Timing")]
+	public float setSerialPortRefreshPeriod = 0.01f; // Seconds
+	public float setSerialPortRequestDelayPeriod = 0.0005f; // Seconds
+
+	public static float SERIAL_PORT_REFRESH_PERIOD;
+	public static float SERIAL_PORT_REQUEST_DELAY_PERIOD;
+
+	// ------------------------- SERIAL PORT -------------------------
+
+	private SerialPort serialPort;
+
+	// ------------------------- DATA -------------------------
+
+	public static string currentArduinoDataPacket = "";
+
+	// ------------------------- AWAKE -------------------------
+
+	void Awake() {
+
+		// Serial port
+		COM_PORT = setCOMPort;
+		COM_BAUD = setCOMBaud;
+
+		// Timing
+		SERIAL_PORT_REFRESH_PERIOD = setSerialPortRefreshPeriod;
+		SERIAL_PORT_REQUEST_DELAY_PERIOD = setSerialPortRequestDelayPeriod;
+
+	}
+
+	// ------------------------- START -------------------------
+
+	void Start () {
+
+		// Setup COM port
+		string comPortName = ArduinoInterface.COM_PORT;
+		serialPort = new SerialPort (comPortName, ArduinoInterface.COM_BAUD);
+		serialPort.Open ();
+
+		// Start getting data
+		if (serialPort.IsOpen) {
+			StartCoroutine ("getSerialData");
+			Debug.Log ("Opened " + comPortName);
+		} else {
+			Debug.LogError ("Unable to open " + comPortName);
+		}
+
+	}
+
+	// ------------------------- GET SERIAL PORT DATA -------------------------
+
+	IEnumerator getSerialData() {
+
+		yield return new WaitForSeconds (SERIAL_PORT_REFRESH_PERIOD);
+
+		if (serialPort != null) {
+
+			// Request data
+			Debug.Log ("Requesting data...");
+			serialPort.Write ("1");
+			yield return new WaitForSeconds (SERIAL_PORT_REQUEST_DELAY_PERIOD);
+
+			// Read the current data packet
+			currentArduinoDataPacket = serialPort.ReadLine ();
+
+			// Continue to get more data
+			StartCoroutine ("getSerialData");
+
+		}
+
+	}
+
+}
--- Unity/hand import/Assets/Scripts/Rotate_Palm.cs
+++ Unity/hand import/Assets/Scripts/Rotate_Palm.cs
@@ -1,18 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO.Ports;
 
 public class Rotate_Palm : MonoBehaviour {
-
-	// ------------------------- SERIAL PORT -------------------------
-
-	SerialPort serialPort;
-
-	// ------------------------- TIMING -------------------------
-
-	float serialPortRefreshPeriod;
-	float serialPortRequestDelayPeriod;
 
 	// ------------------------- ORIENTAION -------------------------
 
@@ -38,7 +28,7 @@ public class Rotate_Palm : MonoBehaviour {
 	public Vector3 offsetRotaion = new Vector3(90f, 0f, 90f);
 	public Vector3 additionalRotaion = new Vector3(0f, 0f, 0f);
 	Quaternion centerQuaternion = Quaternion.identity;
-	public bool setSenterRotaionNow = false;
+	public bool setSenterRotationNow = false;
 
 	float[] caliRotation = { 0, 0, 0 };
 
@@ -55,180 +45,153 @@ public class Rotate_Palm : MonoBehaviour {
 	// horizontal hand position, want to update later to being down
 	// Use this for initialization
 	void Start () {
-
-		// Setup timing
-		serialPortRefreshPeriod = Constants.SERIAL_PORT_REFRESH_PERIOD;
-		serialPortRequestDelayPeriod = Constants.SERIAL_PORT_REQUEST_DELAY_PERIOD;
-
-		// Setup COM port
-		string comPortName = Constants.COM_PORT;
-		serialPort = new SerialPort (comPortName, Constants.COM_BAUD);
-		serialPort.Open ();
-
-		// Start getting data
-		if (serialPort.IsOpen) {
-			StartCoroutine ("getSerialData");
-			Debug.Log ("Opened " + comPortName);
-		} else {
-			Debug.LogError ("Unable to open " + comPortName);
-		}
-
+		StartCoroutine ("ProcessRotation");
 	}
 
 	// ------------------------- GET SERIAL PORT DATA -------------------------
 
-	IEnumerator getSerialData() {
+	IEnumerator ProcessRotation() {
 
-		yield return new WaitForSeconds (serialPortRefreshPeriod);
+		yield return new WaitForSeconds (ArduinoInterface.SERIAL_PORT_REFRESH_PERIOD);
 
-		if(serialPort != null) {
+		// Assume data will be valid
+		bool isValidData = true;
 
-			// Request data
-			Debug.Log("Requesting data...");
-			serialPort.Write("1");
-			yield return new WaitForSeconds (serialPortRequestDelayPeriod);
+		// Get the data
+		string currentArduinoDataPacket = ArduinoInterface.currentArduinoDataPacket;
+		// Split the line at each comma (this is how the code currently formats the output)
+		string[] vec3 = currentArduinoDataPacket.Split (',');
 
-			// Assume data will be valid
-			bool isValidData = true;
+		// Check that there are all 4 parts
+		if (vec3.Length < 4) {
+			isValidData = false;
+		}
 
-			//Read currnet orientaion from Arduino
-			string line = serialPort.ReadLine ();
-			//Debug.Log("Got data: " + line);
-			// Split the line at each comma (this is how the code currently formats the output)
-			string[] vec3 = line.Split (',');
+		// If the data is valid, process it
+		if (isValidData) {
 
-			// Check that there are all 4 parts
-			if (vec3.Length < 4) {
-				isValidData = false;
+			float w = float.Parse (vec3 [0]) * (negativeW ? -1f : 1f);
+			float x = float.Parse (vec3 [1]) * (negativeX ? -1f : 1f);
+			float y = float.Parse (vec3 [2]) * (negativeY ? -1f : 1f);
+			float z = float.Parse (vec3 [3]) * (negativeZ ? -1f : 1f);
+
+			// Construct input quaternion from read data
+			Quaternion inputQuaternion = Quaternion.identity;
+
+			if(orientaionMode == OrientaionMode.X_Y_Z) {
+				inputQuaternion = new Quaternion (x, y, z, w);
+			} else if(orientaionMode == OrientaionMode.X_Z_Y) {
+				inputQuaternion = new Quaternion (x, z, y, w);
+			} else if(orientaionMode == OrientaionMode.Y_X_Z) {
+				inputQuaternion = new Quaternion (y, x, z, w);
+			} else if(orientaionMode == OrientaionMode.Y_Z_X) {
+				inputQuaternion = new Quaternion (y, z, x, w);
+			} else if(orientaionMode == OrientaionMode.Z_X_Y) {
+				inputQuaternion = new Quaternion (z, x, y, w);
+			} else if(orientaionMode == OrientaionMode.Z_Y_X) {
+				inputQuaternion = new Quaternion (z, y, x, w);
 			}
 
-			// If the data is valid, process it
-			if (isValidData) {
-
-				float w = float.Parse (vec3 [0]) * (negativeW ? -1f : 1f);
-				float x = float.Parse (vec3 [1]) * (negativeX ? -1f : 1f);
-				float y = float.Parse (vec3 [2]) * (negativeY ? -1f : 1f);
-				float z = float.Parse (vec3 [3]) * (negativeZ ? -1f : 1f);
-
-				// Construct input quaternion from read data
-				Quaternion inputQuaternion = Quaternion.identity;
-
-				if(orientaionMode == OrientaionMode.X_Y_Z) {
-					inputQuaternion = new Quaternion (x, y, z, w);
-				} else if(orientaionMode == OrientaionMode.X_Z_Y) {
-					inputQuaternion = new Quaternion (x, z, y, w);
-				} else if(orientaionMode == OrientaionMode.Y_X_Z) {
-					inputQuaternion = new Quaternion (y, x, z, w);
-				} else if(orientaionMode == OrientaionMode.Y_Z_X) {
-					inputQuaternion = new Quaternion (y, z, x, w);
-				} else if(orientaionMode == OrientaionMode.Z_X_Y) {
-					inputQuaternion = new Quaternion (z, x, y, w);
-				} else if(orientaionMode == OrientaionMode.Z_Y_X) {
-					inputQuaternion = new Quaternion (z, y, x, w);
-				}
-
-				// TODO: flip the axis of the rotaion to match the sensor
-				/*
-				// Convert to euler angles and flip axis
-				Vector3 inputAngles = inputQuaternion.eulerAngles;
-				Vector3 resultAngles = new Vector3 (inputAngles.y, inputAngles.z, inputAngles.x);
-
-				// Construct resultant quaternion
-				Quaternion resultQuaternion = Quaternion.Euler (resultAngles);
-				*/
-
-				if (setSenterRotaionNow) {
-					setSenterRotaionNow = false;
-					centerQuaternion = inputQuaternion;
-					centerQuaternion = Quaternion.Inverse (centerQuaternion);
-				}
-
-				Quaternion offsetQuaternion = Quaternion.Euler (offsetRotaion);
-				//inputQuaternion = inputQuaternion * offsetQuaternion;
-
-				// Set the final orientaion
-				transform.localRotation = inputQuaternion * centerQuaternion;
-				transform.Rotate (offsetRotaion, Space.Self);
-
-				/*
-				transform.RotateAround (transform.right, Mathf.Deg2Rad * additionalRotaion.x);
-				transform.RotateAround (transform.up, Mathf.Deg2Rad * additionalRotaion.y);
-				transform.RotateAround (transform.forward, Mathf.Deg2Rad * additionalRotaion.z);
-				*/
-
-				Vector3 currentEulerAngles = transform.localEulerAngles;
-				//Vector3 currentEulerAngles = transform.InverseTransformDirection(inputQuaternion.eulerAngles);
-				if (invertX) {
-					currentEulerAngles.x = -currentEulerAngles.x;
-					//transform.RotateAround (transform.right, Mathf.PI);
-				}
-				if (invertY) {
-					currentEulerAngles.y = -currentEulerAngles.y;
-					transform.RotateAround (transform.up, Mathf.PI);
-				}
-				if (invertZ) {
-					currentEulerAngles.z = -currentEulerAngles.z;
-					transform.RotateAround (transform.forward, Mathf.PI);
-				}
-				transform.localEulerAngles = currentEulerAngles;
-
-			}
-
+			// TODO: flip the axis of the rotaion to match the sensor
 			/*
-			if (vec3 [0] != "" && vec3 [1] != "" && vec3 [2] != "" && vec3 [3] != "") { //check that no values are blank
-				if (vec3 [0] == "2") { // delimiter representing calibration
-					caliRotation [0] = float.Parse (vec3 [1]);
-					caliRotation [1] = float.Parse (vec3 [2]);
-					caliRotation [2] = float.Parse (vec3 [3]);
+			// Convert to euler angles and flip axis
+			Vector3 inputAngles = inputQuaternion.eulerAngles;
+			Vector3 resultAngles = new Vector3 (inputAngles.y, inputAngles.z, inputAngles.x);
 
-					Debug.Log (caliRotation);
-
-					transform.localEulerAngles = new Vector3 (xo, yo, zo); // set to original hand rotations if your attaching it to forearm set it to 0,0,0,
-					// What we had before:
-             *      //transform.localEulerAngles=new Vector3(
-                    //float.Parse(vec3[1]) - caliRotation[0],
-                    //float.Parse(vec3[2]) - caliRotation[1],
-                    //float.Parse(vec3[3]) - caliRotation[2]);
-
-				} else if (vec3 [0] == "1") { // delimiter representing rotation
-
-					x = float.Parse (vec3 [1]) - caliRotation [0] + xo;
-					y = float.Parse (vec3 [2]) - caliRotation [1] + yo;
-					z = float.Parse (vec3 [3]) - caliRotation [2] + zo;
-					//if (x > 360 || y > 360 || z > 360) {
-	                //    Debug.Log("error euler larger than 360");
-	                //    x = 359; y = 359; z = 359;
-	                //}
-					// transform.localEulerAngles = new Vector3(x, y, z);                        //parse each value from a string to a float
-
-					Quaternion currentRotation = Quaternion.Euler (x, y, z);
-					transform.localRotation = currentRotation;
-				}
-				//transform.Rotate()
-				Debug.Log (vec3);
-
-		        //else (vec3[0] == "0") { // delimiter representing translation
-		        //    transform.Translate(
-		        //        float.Parse(vec3[0]) - lastPosition[0], // - caliRotation[0],
-		        //            float.Parse(vec3[1]) - lastPosition[1], //- caliRotation[1],
-		        //            float.Parse(vec3[2]) - lastPosition[2], //- caliRotation[2],
-		        //            Space.Self
-		        //            );
-		        //    lastPosition[0] = float.Parse(vec3[0]); //set new values for the most recent rotation
-		        //    lastPosition[1] = float.Parse(vec3[1]);
-		        //    lastPosition[2] = float.Parse(vec3[2]);
-		        //    sp.BaseStream.Flush();
-		        //}
-
-			}
+			// Construct resultant quaternion
+			Quaternion resultQuaternion = Quaternion.Euler (resultAngles);
 			*/
 
-			// Continue to get more data
-			StartCoroutine ("getSerialData");
+			if (setSenterRotationNow) {
+				setSenterRotationNow = false;
+				centerQuaternion = inputQuaternion;
+				centerQuaternion = Quaternion.Inverse (centerQuaternion);
+			}
+
+			Quaternion offsetQuaternion = Quaternion.Euler (offsetRotaion);
+			//inputQuaternion = inputQuaternion * offsetQuaternion;
+
+			// Set the final orientaion
+			transform.localRotation = inputQuaternion * centerQuaternion;
+			transform.Rotate (offsetRotaion, Space.Self);
+
+			/*
+			transform.RotateAround (transform.right, Mathf.Deg2Rad * additionalRotaion.x);
+			transform.RotateAround (transform.up, Mathf.Deg2Rad * additionalRotaion.y);
+			transform.RotateAround (transform.forward, Mathf.Deg2Rad * additionalRotaion.z);
+			*/
+
+			Vector3 currentEulerAngles = transform.localEulerAngles;
+			//Vector3 currentEulerAngles = transform.InverseTransformDirection(inputQuaternion.eulerAngles);
+			if (invertX) {
+				currentEulerAngles.x = -currentEulerAngles.x;
+				//transform.RotateAround (transform.right, Mathf.PI);
+			}
+			if (invertY) {
+				currentEulerAngles.y = -currentEulerAngles.y;
+				//transform.RotateAround (transform.up, Mathf.PI);
+			}
+			if (invertZ) {
+				currentEulerAngles.z = -currentEulerAngles.z;
+				//transform.RotateAround (transform.forward, Mathf.PI);
+			}
+			transform.localEulerAngles = currentEulerAngles;
 
 		}
 
+		/*
+		if (vec3 [0] != "" && vec3 [1] != "" && vec3 [2] != "" && vec3 [3] != "") { //check that no values are blank
+			if (vec3 [0] == "2") { // delimiter representing calibration
+				caliRotation [0] = float.Parse (vec3 [1]);
+				caliRotation [1] = float.Parse (vec3 [2]);
+				caliRotation [2] = float.Parse (vec3 [3]);
+
+				Debug.Log (caliRotation);
+
+				transform.localEulerAngles = new Vector3 (xo, yo, zo); // set to original hand rotations if your attaching it to forearm set it to 0,0,0,
+				// What we had before:
+         *      //transform.localEulerAngles=new Vector3(
+		//float.Parse(vec3[1]) - caliRotation[0],
+		//float.Parse(vec3[2]) - caliRotation[1],
+		//float.Parse(vec3[3]) - caliRotation[2]);
+
+		} else if (vec3 [0] == "1") { // delimiter representing rotation
+
+			x = float.Parse (vec3 [1]) - caliRotation [0] + xo;
+			y = float.Parse (vec3 [2]) - caliRotation [1] + yo;
+			z = float.Parse (vec3 [3]) - caliRotation [2] + zo;
+			//if (x > 360 || y > 360 || z > 360) {
+			//    Debug.Log("error euler larger than 360");
+			//    x = 359; y = 359; z = 359;
+			//}
+			// transform.localEulerAngles = new Vector3(x, y, z);                        //parse each value from a string to a float
+
+			Quaternion currentRotation = Quaternion.Euler (x, y, z);
+			transform.localRotation = currentRotation;
+		}
+		//transform.Rotate()
+		Debug.Log (vec3);
+
+		//else (vec3[0] == "0") { // delimiter representing translation
+		//    transform.Translate(
+		//        float.Parse(vec3[0]) - lastPosition[0], // - caliRotation[0],
+		//            float.Parse(vec3[1]) - lastPosition[1], //- caliRotation[1],
+		//            float.Parse(vec3[2]) - lastPosition[2], //- caliRotation[2],
+		//            Space.Self
+		//            );
+		//    lastPosition[0] = float.Parse(vec3[0]); //set new values for the most recent rotation
+		//    lastPosition[1] = float.Parse(vec3[1]);
+		//    lastPosition[2] = float.Parse(vec3[2]);
+		//    sp.BaseStream.Flush();
+		//}
+
 	}
+	*/
+
+	// Continue to get more data
+	StartCoroutine ("ProcessRotation");
+
+}
 
 	// Update is called once per frame
 	void Update ()	{
